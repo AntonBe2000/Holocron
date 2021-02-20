d8b76ef4e8576818832307922ce992545260c915
AntonBe2000 <anton.b@live.de>
2021-02-20 21:03:14 +0100

    Mailserver hinzugefügt

--- SurveyJS/public/modal_content.php
+++ SurveyJS/public/modal_content.php
@@ -3,7 +3,22 @@
     <h2>Kontaktaufnahme</h2>
 </div>
 <div class="modal-body">
-    <form name="meldung" id="meldung" method="post" action="meldung_auswertung.php" enctype="multipart/form-data">
+    <form name="meldung" id="meldung" method="post" action="
+<?php
+    //Parameter der Meldung abfragen
+    $vorname = $_POST["vorname"]; //Vorname
+    $nachname = $_POST["nachname"]; //Nachname
+    $nachricht = $_POST["nachricht"]; //E-Mail Adresse
+    $email = $_POST["email"]; //Vorname
+    //E-Mail vorbereiten
+    $betreff = "Holocron Nachricht von ".$vorname." ".$nachname;
+    $headers = "From: holocron@anton-bechtloff.de" . "\r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/plain;charset=utf-8";
+    //Mailfunktion aufrufen
+    mail($email, $betreff, $nachricht, $headers);
+?>
+" enctype="multipart/form-data">
         <table>
             <tbody>
             <tr>
@@ -39,7 +54,8 @@
         </table>
         <div>
             <?php
-            require_once __DIR__ . "/meldung_auswertung.php";?>
+            require_once __DIR__ . "/meldung_auswertung.php";
+            ?>
         </div>
 
     </form>
