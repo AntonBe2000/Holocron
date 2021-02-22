cf1904f56fcddecb385eeb139d7da31a9edd1667
AntonBe2000 <anton.b@live.de>
2021-02-22 16:33:49 +0100

    QuestionWithUnknown.php und QuestionDocu.php gelöscht

--- SurveyJS/src/Questions/QuestionWithUnknown.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-abstract class QuestionWithUnknown
-{
-    const FACTOR_SCALE = 10;
-    const QUESTION_SCALE = 5;
-
-    abstract public function calculate($value, &$factors,&$excluded);
-}
