ae7372777ca4db5a78531aa231c7302a64f2b783
AntonBe2000 <anton.b@live.de>
2021-02-23 19:48:31 +0100

    glossar.php hinzugefügt

---
+++ SurveyJS/public/glossar.php
@@ -0,0 +1,12 @@
+<?php include("header.php");?>
+<ul class="topnav">
+    <li><img src="assets/nav_holocron.png" alt="bild" height="36" width="40" id="navbar_image"></li>
+    <li><a href="index.php">Preismodellberatung</a></li>
+    <li><a class="active" href="glossar.php">Glossar</a></li>
+    <li><a href="modelle_info.php">Übersicht über Preismodelle</a></li>
+    <!-- <li class="right"><a href="#">Logout</a></li> -->
+</ul>
+<main>
+Hier entsteht ein Glossar.
+
+<?php include ("footer.html"); ?>
--- SurveyJS/public/index.php
+++ SurveyJS/public/index.php
@@ -6,6 +6,7 @@
 <ul class="topnav">
     <li><img src="assets/nav_holocron.png" alt="bild" height="36" width="40" id="navbar_image"></li>
     <li><a class="active" href="index.php">Preismodellberatung</a></li>
+    <li><a href="glossar.php">Glossar</a></li>
     <li><a href="modelle_info.php">Übersicht über Preismodelle</a></li>
     <!-- <li class="right"><a href="#">Logout</a></li> -->
 </ul>
