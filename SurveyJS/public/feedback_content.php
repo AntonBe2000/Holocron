610e4cf73654e99793287bd51180800419d35be8
AntonBe2000 <anton.b@live.de>
2021-02-23 08:32:31 +0100

    Feedbackfunktion version 1 hinzugefügt

---
+++ SurveyJS/public/feedback_content.php
@@ -0,0 +1,40 @@
+<div class="modal-header">
+    <span class="close">&times;</span>
+    <h2>Feedback</h2>
+</div>
+<div class="modal-body">
+    <form name="feedback" id="feedback" method="post" action="
+<?php
+    //Parameter der Meldung abfragen
+    $akro = $_POST["akro"]; //Akronym
+    $nachricht = $_POST["nachricht"]; //Nachricht
+    //E-Mail vorbereiten
+    $betreff = "Holcron Feedback von " . $akro;
+    $headers = "From: holocron@anton-bechtloff.de" . "\r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/plain;charset=utf-8";
+    //Mailfunktion aufrufen
+    mail("Anton.Bechtloff@deutschebahn.com", $betreff, $nachricht, $headers);
+    ?>
+    " enctype="multipart/form-data">
+        <table>
+            <tbody>
+            <tr>
+                <td><label for="akro">Namensakronym</label></td>
+                <td><input type="text" placeholder="Max" name="akro" id="akro" required></td>
+            </tr>
+            <tr>
+                <td><label for="nachricht">Feedback</label></td>
+                <td><textarea placeholder="Message..." rows="7" name="nachricht" id="nachricht" required></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button type="submit" class="button">Absenden</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+</div>
--- SurveyJS/public/index.php
+++ SurveyJS/public/index.php
@@ -10,9 +10,8 @@
     <!-- <li class="right"><a href="#">Logout</a></li> -->
 </ul>
 <main>
-
 <div x-data="surveyData()" x-init="init()">
-    <div id="surveyElement"></div>
+    <div id="surveyElement"></div>                            
     <div id="surveyResultElement"></div>
     <script src="scripts/index.js"></script>
     <div id="surveyResult" x-show="Object.entries(preismodell).length > 0">
@@ -120,9 +119,7 @@
                 </div>
             </template>
         </div>
-
         <!-- Kontaktaufnahmebereich -->
-
         <!-- Trigger/Open The Modal -->
         <button class="button" id="myBtn">Kontakt aufnehmen</button>
         <!-- The Modal -->
@@ -132,7 +129,6 @@
                 <?php include("modal_content.php"); ?>
             </div>
         </div>
-
         <script>
             // Get the modal
             var modal = document.getElementById("myModal");
@@ -155,13 +151,45 @@
                 }
             }
         </script>
-
         <!--Ende Kontaktaufnahmebereich-->
 
 
     </div>
 </div>
+    <!-- Feedbackfunktion -->
+    <!-- Trigger/Open The Modal -->
+    <div class="btn" onclick="#" id="feedbackButton"><img src="assets/questionTag.png" width="99" height="99" alt="?"></div>
+
+    <!-- The Modal -->
+    <div id="feedbackModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <?php include("feedback_content.php"); ?>
+        </div>
     </div>
+    <script>
+        // Get the modal
+        var modal = document.getElementById("feedbackModal");
+        // Get the button that opens the modal
+        var btn = document.getElementById("feedbackButton");
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks the button, open the modal
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+    <!-- Ende Feedbackfunktion -->
 
 
     <?php include("footer.html"); ?>
