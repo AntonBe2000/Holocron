bc4c8ce747cd85ee61a8cfe7b22bd38c6ea96694
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 18:58:11 +0100

    kundensit_hoehe_risiko implementiert.

--- SurveyJS/src/Questions/Kundensituation/Kundensit_hoehe_risiko.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_hoehe_risiko.php
@@ -6,6 +6,26 @@ class Kundensit_hoehe_risiko extends Question
 
     public function calculate($value, &$factors, &$excluded)
     {
-        // TODO: Implement calculate() method.
+        switch ($value)
+        {
+            case "1":
+                $factors["Risiko Systel"][] = 10;
+                break;
+            case "2":
+                $factors["Risiko Systel"][] = 7;
+                $factors["Risiko Kunde"][] = 3;
+                break;
+            case "3":
+                $factors["Risiko Systel"][] = 5;
+                $factors["Risiko Kunde"][] = 5;
+                break;
+            case "4":
+                $factors["Risiko Systel"][] = 3;
+                $factors["Risiko Kunde"][] = 7;
+                break;
+            case "5":
+                $factors["Risiko Kunde"][] = 10;
+                break;
+        }
     }
 }
