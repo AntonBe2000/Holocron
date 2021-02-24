9cb9b4a2db50189dd340a3175201484da52f87da
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 19:05:57 +0100

    kundensit_tragen_sie_risiko implementiert

--- SurveyJS/src/Questions/Kundensituation/Kundensit_tragen_sie_risiko.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_tragen_sie_risiko.php
@@ -5,6 +5,24 @@ class Kundensit_tragen_sie_risiko extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        //bitte implementieren
+        switch ($value)
+        {
+            case "Ja, alle":
+                $factors["Risiko Systel"][] = 10;
+                $factors["Anzahl Nutzergruppen"][] = 5;
+                break;
+            case "Die meisten":
+                $factors["Risiko Systel"][] = 7;
+                $factors["Anzahl Nutzergruppen"][] = 4;
+                break;
+            case "Die wenigsten":
+                $factors["Risiko Systel"][] = 5;
+                $factors["Anzahl Nutzergruppen"][] = 2;
+                break;
+            case "Nein, keiner":
+                $factors["Risiko Systel"][] = 3;
+                $factors["Anzahl Nutzergruppen"][] = 1;
+                break;
+        }
     }
 }
