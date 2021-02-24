7d663bb700d119d2bf5025ece4393706a289791c
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 19:27:04 +0100

    Kundensit_kerngeschaeft_korreliert implementiert

--- SurveyJS/src/Questions/Kundensituation/Kundensit_kerngeschaeft_korreliert.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_kerngeschaeft_korreliert.php
@@ -6,6 +6,30 @@ class Kundensit_kerngeschaeft_korreliert extends Question
 
     public function calculate($value, &$factors, &$excluded)
     {
-        // TODO: Implement calculate() method.
+        switch ($value)
+        {
+            case "1":
+                $factors["Risiko Systel"][] = 10;
+                $factors["Preisbereitschaft"][] = -5;
+                break;
+            case "2":
+                $factors["Risiko Systel"][] = 7;
+                $factors["Preisbereitschaft"][] = -2;
+                $factors["Budgetsituation Kunde"][] = 3;
+                break;
+            case "3":
+                $factors["Risiko Systel"][] = 5;
+                $factors["Budgetsituation Kunde"][] = 5;
+                break;
+            case "4":
+                $factors["Risiko Systel"][] = 3;
+                $factors["Preisbereitschaft"][] = 2;
+                $factors["Budgetsituation Kunde"][] = 7;
+                break;
+            case "5":
+                $factors["Preisbereitschaft"][] = 5;
+                $factors["Budgetsituation Kunde"][] = 10;
+                break;
+        }
     }
 }
