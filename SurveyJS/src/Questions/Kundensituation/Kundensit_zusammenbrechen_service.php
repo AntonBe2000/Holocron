2112671b8da76ad5e70c12a1dd6db143b4a0ae31
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 19:18:30 +0100

    kundensit_zusammenbrechen_service geändert von esoterischem Umrechnungskram hin zu Werten aus der Matrix

--- SurveyJS/src/Questions/Kundensituation/Kundensit_zusammenbrechen_service.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_zusammenbrechen_service.php
@@ -5,8 +5,32 @@ class Kundensit_zusammenbrechen_service extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        $factors["Preisbereitschaft"][] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
-        $factors["Risiko"][] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
-        $factors["Nutzungsabhängig"][] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
+        switch ($value)
+        {
+            case "1":
+                $factors["Risiko Systel"][] = 10;
+                $factors["Preisbereitschaft"][] = 2;
+                $factors["Bedürfnishäufigkeit"][] = 2;
+                break;
+            case "2":
+                $factors["Risiko Systel"][] = 7;
+                $factors["Preisbereitschaft"][] = 3;
+                $factors["Bedürfnishäufigkeit"][] = 4;
+                break;
+            case "3":
+                $factors["Risiko Systel"][] = 5;
+                $factors["Preisbereitschaft"][] = 4;
+                $factors["Bedürfnishäufigkeit"][] = 6;
+                break;
+            case "4":
+                $factors["Risiko Systel"][] = 3;
+                $factors["Preisbereitschaft"][] = 6;
+                $factors["Bedürfnishäufigkeit"][] = 8;
+                break;
+            case "5":
+                $factors["Preisbereitschaft"][] = 7;
+                $factors["Bedürfnishäufigkeit"][] = 10;
+                break;
+        }
     }
 }
