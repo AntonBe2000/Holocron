fa3773462c527687809b6e935677b33b13a9a41e
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 17:15:04 +0100

    kunden_wie_viele implementiert

--- SurveyJS/src/Questions/Kunden/Kunden_wie_viele.php
+++ SurveyJS/src/Questions/Kunden/Kunden_wie_viele.php
@@ -5,6 +5,26 @@ class Kunden_wie_viele extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        //bitte implementieren
+        switch ($value)
+        {
+            case "1":
+                $factors["Risiko Systel"][] = -5;
+                break;
+            case "2-10":
+                $factors["Risiko Systel"][] = -2;
+                $factors["Anzahl Nutzergruppen"][] = 3;
+                $factors["Nutzungserfassung"][] = 2;
+                break;
+            case "10-100":
+                $factors["Risiko Systel"][] = 2;
+                $factors["Anzahl Nutzergruppen"][] = 7;
+                $factors["Nutzungserfassung"][] = 5;
+                break;
+            case "Mehr als 100":
+                $factors["Risiko Systel"][] = 5;
+                $factors["Anzahl Nutzergruppen"][] = 10;
+                $factors["Nutzungserfassung"][] = 8;
+                break;
+        }
     }
 }
