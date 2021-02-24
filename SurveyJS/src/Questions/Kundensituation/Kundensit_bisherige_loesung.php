40a22fc0d76cdfed1f85b40e332a3e8dfebbecf4
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-24 19:48:12 +0100

    kundensit_bisherige_loesung implementiert

--- SurveyJS/src/Questions/Kundensituation/Kundensit_bisherige_loesung.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_bisherige_loesung.php
@@ -5,6 +5,22 @@ class Kundensit_bisherige_loesung extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        // $factors["Preisbereitschaft"] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
+        switch ($value)
+        {
+            case "Manueller Prozess":
+                $factors["Nutzer"][] = 3;
+                $factors["Risiko Kunde"][] = -5;
+                $factors["Preisbereitschaft"][] = 7;
+                break;
+            case "Andere IT-Lösung":
+                $factors["Nutzer"][] = 5;
+                $factors["Risiko Systel"][] = 3;
+                $factors["Preisbereitschaft"][] = -3;
+                $factors["Budgetsituation Kunde"][] = 3;
+                break;
+            case "Sonstige, und zwar...":
+                // Derzeit nichts zu tun
+                break;
+        }
     }
 }
