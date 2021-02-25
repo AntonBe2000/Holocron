028f288b0baacbe6cb598764154e3e594de01f31
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-25 14:06:24 +0100

    Netzwerk_gewinnbeteiligung angepasst so dass im Falle von "Nein" das inhaltsbasierte Preismodell ausgeschlossen wird.

--- SurveyJS/src/Questions/Netzwerk/Netzwerk_gewinnbeteiligung.php
+++ SurveyJS/src/Questions/Netzwerk/Netzwerk_gewinnbeteiligung.php
@@ -6,5 +6,11 @@ class Netzwerk_gewinnbeteiligung extends Question
     public function calculate($value, &$factors, &$excluded)
     {
         //erst sinnvoll wenn Value-Sharing als Preismodell mit aufgenommen wird
+        switch ($value)
+        {
+            case "Nein": 
+                $excluded["Inhaltsbasiert"] = "Dieses Preismodell wurde ausgeschlossen, weil die Mehrzahl der Kunden keine Beteiligung an ihrem Gewinn anbieten.";
+                break;
+        }
     }
 }
