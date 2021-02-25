ce9a5a9c91dbab1db1d77b7e192df7d97f7a73ab
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-25 09:15:38 +0100

    Implementierung von Nutzen_wie_haeufig geändert nach Excel-Entwurf

--- SurveyJS/src/Questions/Kundennutzen/Nutzen_wie_haeufig.php
+++ SurveyJS/src/Questions/Kundennutzen/Nutzen_wie_haeufig.php
@@ -5,7 +5,23 @@ class Nutzen_wie_haeufig extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        // TODO: Implement calculate() method.
-        $factors["Bedürfnishäufigkeit"][] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
+        switch ($value)
+        {
+            case "1":
+                $factors["Bedürfnishäufigkeit"][] = 1;
+                break;
+            case "2":
+                $factors["Bedürfnishäufigkeit"][] = 3;
+                break;
+            case "3":
+                $factors["Bedürfnishäufigkeit"][] = 5;
+                break;
+            case "4":
+                $factors["Bedürfnishäufigkeit"][] = 8;
+                break;
+            case "5":
+                $factors["Bedürfnishäufigkeit"][] = 10;
+                break;
+        }
     }
 }
