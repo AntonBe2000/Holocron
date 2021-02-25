ca1c28dbb0e0e8227cb137f166fe3854c4e6e0dc
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-25 13:56:46 +0100

    Kundensit_budgetsituation an Werte aus Excel angepasst.

--- SurveyJS/src/Questions/Kundensituation/Kundensit_budgetsituation.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_budgetsituation.php
@@ -5,6 +5,28 @@ class Kundensit_budgetsituation extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        $factors["Budgetsituation"][] = ($value / self::QUESTION_SCALE) * self::FACTOR_SCALE;
+        switch ($value)
+        {
+            case "1":
+                $factors["Preisbereitschaft"][] = 1;
+                $factors["Budgetsituation Kunde"][] = 1;
+                break;
+            case "2":                
+                $factors["Preisbereitschaft"][] = 3;
+                $factors["Budgetsituation Kunde"][] = 3;
+                break;
+            case "3":             
+                $factors["Preisbereitschaft"][] = 5;   
+                $factors["Budgetsituation Kunde"][] = 5;
+                break;
+            case "4":
+                $factors["Preisbereitschaft"][] = 8;                
+                $factors["Budgetsituation Kunde"][] = 8;
+                break;
+            case "5":
+                $factors["Preisbereitschaft"][] = 10;               
+                $factors["Budgetsituation Kunde"][] = 10;
+                break;
+        }
     }
 }
