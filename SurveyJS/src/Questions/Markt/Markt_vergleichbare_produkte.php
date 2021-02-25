4967e7fc16f48355774594f30507dd321e2e798c
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-25 09:24:15 +0100

    Markt_vergleichbare_produkte angepasst an Excel-Vorlage

--- SurveyJS/src/Questions/Markt/Markt_vergleichbare_produkte.php
+++ SurveyJS/src/Questions/Markt/Markt_vergleichbare_produkte.php
@@ -8,9 +8,11 @@ class Markt_vergleichbare_produkte extends Question
         switch ($value) {
             case "Ja":
                 $factors["Preisbereitschaft"][] = 8;
+                $factors["Modell_am_Markt"][] = 10;
                 break;
             case "Nein":
                 $factors["Preisbereitschaft"][] = 2;
+                $factors["Modell_am_Markt"][] = -10;
                 break;
         }
     }
