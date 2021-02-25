cbd0754b01f4abbe2200d28eab3d8c2e8aff094f
Pilsator <dr.stefan.roth@googlemail.com>
2021-02-25 09:18:20 +0100

    Nutzen_wie_erfolgt gemäß Excel implementiert

--- SurveyJS/src/Questions/Kundennutzen/Nutzen_wie_erfolgt.php
+++ SurveyJS/src/Questions/Kundennutzen/Nutzen_wie_erfolgt.php
@@ -5,12 +5,22 @@ class Nutzen_wie_erfolgt extends Question
 {
     public function calculate($value, &$factors, &$excluded)
     {
-        switch ($value) {
-            case "Kurzfristig und temporär":
-                $factors["Bedürfnishäufigkeit"][]= 8;
+        switch ($value)
+        {
+            case "1":
+                $factors["Bedürfnishäufigkeit"][] = 1;
                 break;
-            case "Dauerhaft":
-                $factors["Bedürfnishäufigkeit"][] = 2;
+            case "2":
+                $factors["Bedürfnishäufigkeit"][] = 3;
+                break;
+            case "3":
+                $factors["Bedürfnishäufigkeit"][] = 5;
+                break;
+            case "4":
+                $factors["Bedürfnishäufigkeit"][] = 8;
+                break;
+            case "5":
+                $factors["Bedürfnishäufigkeit"][] = 10;
                 break;
         }
     }
