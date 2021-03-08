26b1a073debde546cf6f98e3c262b00018b2029f
AntonBe2000 <anton.b@live.de>
2021-03-08 13:01:18 +0100

    Spielseite angelegt

---
+++ SurveyJS/public/test.php
@@ -0,0 +1,12 @@
+<?php include("header.php") ?>
+<ul class="topnav">
+    <li><img src="assets/nav_holocron.png" alt="bild" height="36" width="39" id="navbar_image"></li>
+    <li><a class="active" href="index.php">Beratung</a></li>
+    <li><a href="factorglossary.php">Faktoren</a></li>
+    <li><a href="modelle_info.php">Preismodelle</a></li>
+    <?php if(isset($_SESSION['userid'])) {echo '<li class="right"><a href="logout.php">Logout</a></li>';}?>
+</ul>
+
+
+
+<?php include ("footer.html") ?>
